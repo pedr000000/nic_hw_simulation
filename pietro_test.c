c3a7746ca25a06ed4a8ec6847c5afed0bc7b8a8b
Pietro Paolini <p.paolini@genexis.eu>
2014-05-23 15:36:49 +0200

    module: follow the 3.2 Kernel interface

--- pietro_test.c
+++ pietro_test.c
@@ -1,36 +1,76 @@
-#define MODULE
 #define LINUX
-#define __KERNEL__
 
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/netdevice.h>
 
-#define PRINTPERROR printk(KERN_ERR "Error at %s %s %d", __FILE__, __FUNCTION__, __LINE__);
+#define DNAME "pdmahwsim"
 
-static void *buffer_dma;
 
-static int __init pietrodma_init(void)
+static int pdmahwsim_open(struct net_device *net)
 {
-   printk(KERN_DEBUG "Initalizing %s\n", KBUILD_MODNAME);
-   buffer_dma = (void *) __get_free_pages(GFP_ATOMIC | __GFP_DMA, 3);
-   if (!buffer_dma) {
-   	PRINTPERROR
-	return -ENOMEM;
-   }
-   /* Register a net device */
-   /* IRQ handler */
-   /* start kthread */
-   return 0;
+	return 0;
+}
+
+static int pdmahwsim_stop(struct net_device *net)
+{
+	return 0;
+}
+
+static int pdmahwsim_xmit(struct sk_buff *skb, struct net_device *net)
+{
+	return 0;
+}
+
+static const struct net_device_ops pdmshwsim_ops = {
+	.ndo_open = pdmahwsim_open,
+	.ndo_stop = pdmahwsim_stop,
+	.ndo_start_xmit = pdmahwsim_xmit,
+};
+
+static void pdmahwsim_init(struct net_device *net)
+{
+	ether_setup(net);
+	net->netdev_ops = &pdmshwsim_ops;
+	printk(KERN_DEBUG "Initalizing %s\n", __FUNCTION__);
+	return;
+
+}
+
+/* Split in two files */
+static void *buffer_dma;
+static struct net_device *pdmahw_d;
+static int __init pdmahwsim_init_module(void)
+{
+	int result;
+	printk(KERN_DEBUG "Initalizing %s\n", KBUILD_MODNAME);
+	buffer_dma = (void *) __get_free_pages(GFP_ATOMIC | __GFP_DMA, 3);
+	if (!buffer_dma) {
+		printk(KERN_ERR "%s %s %d", __FILE__, __FUNCTION__, __LINE__);
+		return -ENOMEM;
+	}
+	pdmahw_d = alloc_netdev(64, "pdmahwsim%d", pdmahwsim_init);
+	if (!pdmahw_d)
+		return ENODEV;
+	if ((result = register_netdev(pdmahw_d))) {
+		printk(KERN_ERR "%s %s %d", __FILE__, __FUNCTION__, __LINE__);
+		return result;
+	}
+	/* IRQ handler */
+	/* start kthread */
+	return 0;
 }
 
 
-void pietrodma_exit(void)
+void pdmahwsim_exit_module(void)
 {
-  printk(KERN_ALERT "Goodbye world 1.\n");
+	unregister_netdev(pdmahw_d);
+	free_netdev(pdmahw_d);
+	printk(KERN_ALERT "Goodbye %s\n", KBUILD_MODNAME);
 }
 
 MODULE_LICENSE("GPL");
-module_init(pietrodma_init);
-module_exit(pietrodma_exit);
+module_init(pdmahwsim_init_module);
+module_exit(pdmahwsim_exit_module);
 
